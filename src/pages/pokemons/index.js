8ec9118e8082b31a3e9de50c6f7d86e131b3eea8
selinvik <selinvictor1993@mail.ru>
2023-04-04 14:37:44 +0200

    yarn add material and initial list layout

--- src/pages/pokemons/index.js
+++ src/pages/pokemons/index.js
@@ -1,15 +1,67 @@
 import { useListPokemonsQuery } from 'shared/api/pokemons';
 import PokemonCard from './pokemon-card';
 
+import { Grid, TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    padding: 10
+  },
+  formControl: {
+    margin: 10,
+    minWidth: 120
+  }
+}));
+
 const Pokemons = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [typeFilter, setTypeFilter] = useState('');
+  const classes = useStyles();
+
   const { data: pokemonsList, isSuccess } = useListPokemonsQuery();
+
+  // const filteredPokemons = pokemons
+  //   .filter((pokemon) => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  //   .filter((pokemon) => (typeFilter ? pokemon.types.includes(typeFilter) : true));
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleTypeFilter = (event) => {
+    setTypeFilter(event.target.value);
+  };
 
   if (isSuccess) {
     return (
-      <div>
-        {pokemonsList.results.map((item) => (
-          <PokemonCard key={item.name} url={item.url} />
-        ))}
+      <div className={classes.root}>
+        <FormControl className={classes.formControl}>
+          <TextField
+            id="search"
+            label="Search"
+            variant="outlined"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="type-filter-label">Type</InputLabel>
+          <Select
+            labelId="type-filter-label"
+            id="type-filter"
+            value={typeFilter}
+            onChange={handleTypeFilter}>
+            <MenuItem value="">All Types</MenuItem>
+          </Select>
+        </FormControl>
+        <Grid container spacing={3}>
+          {pokemonsList.results.map((item) => (
+            <PokemonCard key={item.name} url={item.url} />
+          ))}
+        </Grid>
       </div>
     );
   }
--- src/pages/pokemons/pokemon-card/index.js
+++ src/pages/pokemons/pokemon-card/index.js
@@ -1,15 +1,52 @@
 /* eslint-disable react/prop-types */
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { useCurrentPokemonQuery } from 'shared/api/pokemons';
 
+const useStyles = makeStyles(() => ({
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  cardMedia: {
+    paddingTop: '100%'
+  }
+}));
+
 const PokemonCard = ({ url = '' }) => {
-  const { data: pokemon } = useCurrentPokemonQuery(
+  const classes = useStyles();
+  const { data: pokemon, isSuccess } = useCurrentPokemonQuery(
     {
       url
     },
     { skip: !url }
   );
 
-  return <div>{pokemon.name}</div>;
+  if (isSuccess) {
+    return (
+      <Grid item xs={12} sm={6} md={4} key={pokemon.id}>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={pokemon.sprites.front_default}
+            title={pokemon.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {pokemon.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {pokemon.types.join(', ')}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
+
+  return null;
 };
 
 export default PokemonCard;
